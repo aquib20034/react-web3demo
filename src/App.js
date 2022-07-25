2fa274a2340dfc585eff31bcef80294cd5501165
aquib20034 <aquib20034@gmail.com>
2022-07-25 19:03:17 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Web3 from 'web3';
 import { useEffect } from 'react';
+import { CONTRACT_ADDRESS, CONTRACT_ABI } from "./contract/contract";
 
-const ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Paused","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"account","type":"address"}],"name":"Unpaused","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"_address","type":"address"},{"indexed":false,"internalType":"uint256","name":"_quantity","type":"uint256"}],"name":"_burnBox","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"_etherWidthdraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"_publicMinted","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"owner","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"_whitelistMinted","type":"event"},{"inputs":[],"name":"MAX_MINT_LIMIT","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"MAX_SUPPLY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"PUBLIC_SALE_PRICE","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"WHITELIST_SALE_PRICE","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getMerkleRoot","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"isRevealed","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"mint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"pause","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"paused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"publicSale","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes32","name":"_merkleRoot","type":"bytes32"}],"name":"setMerkleRoot","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"_baseTokenUri","type":"string"}],"name":"setTokenUri","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"togglePublicSale","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"toggleReveal","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"toggleWhiteListSale","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"unpause","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"whiteListSale","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"bytes32[]","name":"_merkleProof","type":"bytes32[]"}],"name":"whitelistMint","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]
-
-const ADDRESS  = "0x892c91c02dbab973073c4f42baab873dcf09be1b";
 var account = null;
 var contract = null;
-
+console.log(contract);
 async function connectWallet(){
   try{
     if(window.ethereum){
@@ -18,8 +16,8 @@ async function connectWallet(){
       await window.ethereum.send('eth_requestAccounts');
       var accounts = await web3.eth.getAccounts();
       account = accounts[0];
-      document.getElementById('wallet-address').textContent = account;
-      contract =  new web3.eth.Contract(ABI, ADDRESS);
+      document.getElementById('wallet-address').textContent = "Wallet Address: " + account;
+      contract =  new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
     }
   }catch(ex){
     console.log(ex)
@@ -40,31 +38,42 @@ async function mint(){
   }catch(ex){
     console.log(ex)
   }
-
 }
-
-
 function App() {
   return (
     <div className="App">
       <div className='container'>
-        <div className='row pt-5'>
-          <div className='col-lg-3'></div>
-          <form className='col-lg-6 p-5 ' style={{borderRadius:"5px", boxShadow:"1px 1px 6px #000000"}}>
-            <h4>Mint Portal</h4>
+        <div className='row mt-3'>
+          <div className='col-lg-12 p-3' style={{padding:"15px", borderRadius:"5px",boxShadow:"1px 1px 5px #C9C9C9"}}>
+            <h3 style={{color: "#424242"}}>Web3 Demo React App</h3><hr />
+            <label>It is a web3 demo react application which is connected with ethereum based contract developed on kovan network. This application has two main functions namely "public mint function" whose price is 0.00009 faucet ether wherease "whitelist mint function" whose price is 0.00002 faucet ethers. </label>
+            {/* <h4 style={{color: "#424242"}}>Mint Portal</h4> */}
+          </div>
+        </div>
+        {/* { (contract) ? "test" : "ok"} */}
+        { contract ? "" :
+        <>{
+        <div className='row mt-3'>
+          <div className='col-lg-12 p-3 ' style={{borderRadius:"5px", boxShadow:"1px 1px 5px #C9C9C9"}}>
             <h5>Please connect your wallet</h5>
             <Button onClick ={connectWallet} style={{marginBottom:"5px"}}>Connect Wallet</Button>
-            <div class="card" id="wallet-address" style={{padding:"15px", borderRadius:"5px",boxShadow:"1px 1px 3px #000000"}}>
-              <label for="floatingInput">Wallet Address</label>
-            </div>
-            <div class="card"  style={{marginTop:"5px",padding:"15px", borderRadius:"5px",boxShadow:"1px 1px 3px #000000"}}>
-              <input type="number" name ="amount" defaultValue="1" min="1" max="5" />
-              <label>Please select the amount of NFTs to mint</label>
-              <Button onClick ={mint} >Mint/Buy</Button>
-            </div>
-            <label>Price 0.00009 Eth each mint.</label>
-          </form>
-          <div className='col-lg-3'></div>
+          </div>
+        </div>
+       }</>  }
+        <div className='row mt-3'>
+          <div className='col-lg-12 p-3' id="wallet-address" style={{borderRadius:"5px",boxShadow:"1px 1px 5px #C9C9C9"}}>
+            <label for="floatingInput">Wallet Address: </label>
+          </div>
+        </div>
+        <div className='row mt-3'>
+          <div className='col p-5' style={{borderRadius:"5px", boxShadow:"1px 1px 5px #C9C9C9", marginRight:"6px"}}>
+            <label>Public Mint Price 0.00009 Eth for each mint.</label> <br />
+            <Button onClick ={mint} >Public Mint</Button>
+          </div>
+          <div className='col p-5' style={{borderRadius:"5px", boxShadow:"1px 1px 5px #C9C9C9", marginLeft:"6px"}}>
+          <label>Whitelist Mint Price 0.00003 Eth for each mint.</label> <br />
+            <Button onClick ={mint} >WhiteList Mint</Button><br />
+          </div>
         </div>
       </div>
     </div>
@@ -72,3 +81,15 @@ function App() {
 }
 
 export default App;
+
+
+
+{/* <form className='col p-5' style={{borderRadius:"5px", boxShadow:"1px 1px 6px #000000"}}>
+    <div class="card"  style={{marginTop:"5px",padding:"15px", borderRadius:"5px",boxShadow:"1px 1px 3px #000000"}}>
+      <input type="number" name ="amount" defaultValue="1" min="1" max="5" />
+      <label>Please select the amount of NFTs to mint</label>
+      <Button onClick ={mint} >Mint/Buy</Button>
+    </div>
+    <label>Price 0.00009 Eth each mint.</label>
+  </form> 
+*/}
